495dcff11f2ad89acc5ac66556daaecf2b0800bf
priks <gautamprikshit1@gmail.com>
2021-11-25 11:12:47 +0530

    Minor Changes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+use std::io;
+
 mod password;
 
 fn main() {
     println!("Let's generate a random password for you.");
-    password::generate();
+    println!("Enter the length of required password: ");
+        let mut length = String::new();
+        io::stdin()
+            .read_line(&mut length)
+            .expect("Failed to read line");
+        let length: u32 = length.trim().parse().expect("Please Enter a number");
+
+    match password::generate(length) {
+        Ok(pass) => println!("{}", pass),
+        Err(err) => println!("{}", err)
+    };
 }
--- src/password.rs
+++ src/password.rs
@@ -1,25 +1,15 @@
 use rand::Rng;
-use std::io;
 
-pub fn generate() {
+pub fn generate(length: u32) -> Result<String, &'static str> {
     let chars: String = String::from("!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~");
     let mut pass: String = String::new();
-    loop {
-        println!("Enter the length of required password: ");
-        let mut length = String::new();
-        io::stdin()
-            .read_line(&mut length)
-            .expect("Failed to read line");
-        let length: u32 = length.trim().parse().expect("Please Enter a number");
-        if length < 8 {
-            println!("Way too short for a password! try again.");
-        } else {
-            for _i in 0..length {
-                let index = rand::thread_rng().gen_range(0..chars.len());
-                pass.push(chars.chars().nth(index).unwrap());
-            }
-            println!("Your required password is {}", pass);
-            break;
+    if length < 8 {
+        return Err("Password length should be greater than 8");
+    } else {
+        for _i in 0..length {
+            let index = rand::thread_rng().gen_range(0..chars.len());
+            pass.push(chars.chars().nth(index).unwrap());
         }
-    }
+    return Ok(format!("Your required password is {}", pass));
+}
 }
